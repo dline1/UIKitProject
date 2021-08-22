ff05fd31aac7411771db49b8272addc5b9e8ea00
Артур <89347589+dline1@users.noreply.github.com>
2021-08-22 20:01:45 +0300

    Add files via upload

---
+++ UIKitProject/UIKitProject/UIKitProject/FreeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FreeViewController.swift
+//  UIKitProject
+//
+//  Created by Arthur111 Magomedov on 22.08.2021.
+//
+
+import UIKit
+
+class FreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
