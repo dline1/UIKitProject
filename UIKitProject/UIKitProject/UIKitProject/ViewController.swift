9e0f932a73d755bd838a1098e164d91e0de247ad
Артур <89347589+dline1@users.noreply.github.com>
2021-08-22 19:40:58 +0300

    Add files via upload

---
+++ UIKitProject/UIKitProject/UIKitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitProject
+//
+//  Created by Arthur111 Magomedov on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
